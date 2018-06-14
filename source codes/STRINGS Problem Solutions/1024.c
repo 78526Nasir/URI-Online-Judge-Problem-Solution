0143d34f78d36ca2e96afa73a2c3254da1de95c9
78526Nasir <islam35-1102@diu.edu.bd>
2018-06-14 14:38:09 +0600

    String problem started

---
+++ source codes/STRINGS Problem Solutions/1024.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+#include <string.h>
+#define MAX 1000
+
+int main()
+{
+    int i, n, k=0, len;
+    char plain[MAX], chiper[MAX];
+
+    scanf("%d%*c",&n);
+
+    while(n!=0){
+        scanf("%[^\n]%*c",plain);
+
+        for(i=0; plain[i]!='\0';i++){
+            if(plain[i]>='a' && plain[i]<='z' || plain[i]>='A' && plain[i]<='Z'){
+                plain[i]=plain[i] + 3;
+            }
+        }
+
+        len = strlen(plain);
+
+        for(i=len-1; i>=0; i--)
+            chiper[k++] = plain[i];
+
+        chiper[k] = '\0';
+
+        for(i = len/2; chiper[i]!='\0';i++)
+        {
+            chiper[i]=chiper[i]-1;
+        }
+
+        printf("%s\n",chiper);
+        n--;
+        k=0;
+    }
+
+    return 0;
+}
