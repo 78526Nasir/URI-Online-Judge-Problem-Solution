b153ed9052b15b8b943dc31da69fbd76a56245e8
78526Nasir <islam35-1102@diu.edu.bd>
2018-06-14 15:13:58 +0600

    1016no problem solution added

---
+++ source codes/BEGINNERS Problem Solutions/1016.c
@@ -0,0 +1,16 @@
+
+// solution URI problem : #1016
+
+#include <stdio.h>
+
+int main()
+{
+    int distance;
+
+    scanf("%d", &distance);
+
+    printf("%d minutos\n", (distance*2));
+
+    return 0;
+}
+
