520c4fd81a641439d98139e04775b32fbad4ee9c
78526Nasir <islam35-1102@diu.edu.bd>
2018-06-18 10:51:54 +0600

    solution => 1035

---
+++ source codes/BEGINNERS Problem Solutions/1035.c
@@ -0,0 +1,19 @@
+// solution URI problem : #1035
+
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+
+    if((b>c && d>a) && ((a+b) < (c+d)) && (c>0 && d>0) && (a%2==0))
+    {
+        printf("Valores aceitos\n");
+    }
+    else
+        printf("Valores nao aceitos\n");
+
+    return 0;
+}
+
