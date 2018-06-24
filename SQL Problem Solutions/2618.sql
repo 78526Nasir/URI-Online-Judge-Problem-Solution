ba721d1291ed782a29311466e52f11bf8a704348
78526Nasir <islam35-1102@diu.edu.bd>
2018-06-24 22:37:25 +0600

    2618.sql solution

---
+++ SQL Problem Solutions/2618.sql
@@ -0,0 +1,17 @@
+-- Problem no: 2618 --
+-- solution --
+
+SELECT
+    products.name, providers.name, categories.name
+FROM
+    products
+JOIN
+    providers
+ON
+    products.id_providers = providers.id
+JOIN
+    categories
+ON
+    products.id_categories = categories.id
+WHERE
+    providers.name = 'Sansul SA' AND categories.name = 'Imported'
