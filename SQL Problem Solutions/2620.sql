af655627188703c16f63c1715b70c0077e035f44
78526Nasir <islam35-1102@diu.edu.bd>
2018-06-24 22:37:45 +0600

    2620.sql solution

---
+++ SQL Problem Solutions/2620.sql
@@ -0,0 +1,13 @@
+-- Problem no: 2620 --
+-- solution --
+
+SELECT
+    customers.name, orders.id
+FROM
+    customers
+JOIN
+    orders
+ON
+    orders.id_customers = customers.id
+WHERE 
+    (orders.orders_date >= '2016-01-01' AND orders.orders_date <= '2016-06-30')
