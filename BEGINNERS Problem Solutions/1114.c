909fd256cec5331bbfcc6dd8c29e66bfc924b456
78526Nasir <islam35-1102@diu.edu.bd>
2018-07-09 11:12:07 +0600

    accepted

---
+++ BEGINNERS Problem Solutions/1114.c
@@ -0,0 +1,23 @@
+/// solution no: #1114
+
+#include<stdio.h>
+
+int main()
+{
+    int password;
+    while(1){
+        scanf("%d",&password);
+
+        if(password==2002){
+            printf("Acesso Permitido\n");
+            break;
+        }
+        else{
+            printf("Senha Invalida\n");
+        }
+    }
+
+    return 0;
+}
+
+
